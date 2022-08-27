8d551fa7df243a55035c5c7da69b8dff1d72e0f5
DR1FTERxd <Eligijuskont@gmail.com>
2022-08-27 16:02:03 +0300

    all

--- src/modules/creating-todo.js
+++ src/modules/creating-todo.js
@@ -1,9 +1,43 @@
-let title = document.getElementById('title').value;
-console.log(title);
+
+
+const todoBtn = document.getElementById('btnadd');
+todoBtn.addEventListener('click', addTodo)
+
+
+let title = document.getElementById("title").value;
+let description = document.getElementById("description").value;
+let due_date = document.getElementById("due_date").value;
+let priority = document.getElementById('priority').value;
+
+
+let mytodos = [];
+
+let newTodo;
+
+
+
+
+class Todo {
+    constructor(title, description, due_date, priority) {
+        this.title = title = document.getElementById("title").value;
+        this.description = description = document.getElementById("description").value;
+        this.due_date = due_date = document.getElementById("due_date").value;
+        this.priority = priority = document.getElementById('priority').value;
+    }
+}
+
+function addTodo(event) {
+    event.preventDefault();
+
+    newTodo = new Todo (title, description, due_date, priority)
+    mytodos.push(newTodo)
+    console.log(mytodos)
+    console.log(priority)
+}
+
 
 function work2(){
-    addATodo();
-    createAtodo();
+    addTodo();
 };
 
-export default work2
+export default work2()
